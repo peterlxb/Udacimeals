d766c7d69d9199d81a9b9c2f92ac6574fc5063f6
peterliuxb <woairuidaren@gmail.com>
2017-10-12 16:39:00 +0800

    Using the Provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './components/App';
 import { createStore } from 'redux'
 import reducer from './reducers';
+import { Provider } from 'react-redux'
 
 const store = createStore(reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 
-ReactDOM.render(<App store={store} />,
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
   )
 registerServiceWorker()
