0466e8efa3e97e41462f0890d51e596d0dae05d6
peterliuxb <woairuidaren@gmail.com>
2017-10-12 14:15:00 +0800

    redux dev tools

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './components/App';
 import { createStore } from 'redux'
 import reducer from './reducers';
 
-const store = createStore(reducer)
-console.log(store)
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 
 ReactDOM.render(<App />,
   document.getElementById('root')
